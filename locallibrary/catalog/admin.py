bcec418a6fa6dda02a13ca98f63d8b07a6f4aec4
hiki7 <rsaliyev03@gmail.com>
2024-09-22 19:22:54 +0500

    add: fields attribute to the author model in admin panel and some notes

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,7 +3,10 @@ from .models import Book, BookInstance, Genre, Author, Language
 
 
 class AuthorAdmin(admin.ModelAdmin):
+    #list_display is used for displaying the info in general page
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    #fields is used for displaying the attributes in the form, in which order. Usually, they are displayed vertically, but if we use tuple, we can display horizontally
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 
 @admin.register(Book)
