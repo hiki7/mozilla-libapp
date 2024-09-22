bd492e6b8e2c689a05f12be3a7d918fd8ceab708
hiki7 <rsaliyev03@gmail.com>
2024-09-22 18:31:02 +0500

    add: author model and docstring for book_instance and book models' methods

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -52,6 +52,7 @@ class Book(models.Model):
         return self.title
 
     def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
 
@@ -82,4 +83,24 @@ class BookInstance(models.Model):
         ordering = ['due_back']
 
     def __str__(self):
+        """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
