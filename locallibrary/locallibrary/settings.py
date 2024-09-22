4a286f5686e972223645bb9f34b2814ff1930f8b
hiki7 <rsaliyev03@gmail.com>
2024-09-22 18:50:53 +0500

    add: swagger (just in case)

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
